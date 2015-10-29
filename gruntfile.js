d60bc0e17740a2abbbcf7230068376a457afcb79
John Harden <jharden@pnimedia.com>
2015-10-29 15:32:45 -0700

    updated postcss task to recursively process all files within a specified folder

--- gruntfile.js
+++ gruntfile.js
@@ -11,24 +11,45 @@ module.exports = function(grunt) {
             options:{
                 processors:[
 
-                    require('autoprefixer')({browsers: ['last 1 version']}),
-                    require('cssnext')(),
-                    require('precss')()
+                    require('autoprefixer')({browsers: ['last 8 versions']}),
+                    //require('cssnext')(), not for maintenance projects
+                    //require('precss')(), not for maintenance projects
+                    require('postcss-discard-duplicates'),
+                    require('postcss-discard-empty'),
+                    require('postcss-merge-longhand'),
+                    require('postcss-merge-rules'),
+                    require('postcss-minify-selectors'),
+                    require('postcss-minify-font-values'),
+                    require('postcss-property-sorter')
+                    //require('cssnano')({discardComments: {removeAll: true}})
+
                 ]
             },
 
             //dist points to where css files are read from and written to
             dist:{
-                src: 'postdest/concat.css',
-                dest: 'dest/concat.css'
-            }
+                expand: true,       // Enable dynamic expansion.
+                cwd: 'css/',        // Src matches are relative to this path.
+                src: '**/*.css',    //enable recursive search
+                dest: 'css/',       //set dest file to be same as src file
+                ext: '.css',        //set the file extension
+                extDot: 'first'     //set naming convention
 
+            }
         },
 
         concat:{
             css: {
-                src: ['src/*.css'],
-                dest: 'postdest/concat.css',
+                src: ['css/cards.css','css/checkout.css','css/global.css','css/home.css'],
+                //src: ['css/*.css'],
+                dest: 'css/consolidated-initial.css',
+            }
+        },
+
+        process_css:{
+            target:{
+                src: 'css/*.css',
+                dest: 'css/output/'
             }
         }
 
@@ -39,5 +60,27 @@ module.exports = function(grunt) {
     //create a custom task that uses concat and postcss
     grunt.registerTask('init',['concat','postcss']);
 
+    grunt.registerMultiTask('process_css','runs all css files in a folder through various post processors',function(){
+
+        var done = this.async();
+        grunt.log.write(':: Set async >> ').ok();
+
+
+        this.files.forEach(function(fileList){
+
+            grunt.log.write(':: Processing ' + fileList.src.length + ' files >> ').ok();
+
+            //fileList.src is the list of all matching file names
+
+            fileList.src.forEach(function(file){
+
+                grunt.log.write(':: Processing ' + file + ' >> ').ok();
+                grunt.log.write(':: dest ' + fileList.dest + file +' >> ').ok();
+
+            });
+
+        });
+
+    });
 
 };
